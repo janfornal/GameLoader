23e64dbe3997d7761ffb4927b962338b1c287ae7
Kacper Topolski <kacpertopolski@op.pl>
2022-04-22 23:09:00 +0200

    Added Server.java

---
+++ src/main/java/GameLoader/server/Server.java
@@ -0,0 +1,26 @@
+package GameLoader.server;
+
+import GameLoader.client.Game;
+import GameLoader.common.messages.Message;
+
+public class Server {
+    public static void main(String[] args) {
+
+    }
+
+    void receivedMessage(Message m) {
+
+    }
+
+    void reportGameEnded(Game g) {
+
+    }
+
+    class ListeningConnection implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+}
