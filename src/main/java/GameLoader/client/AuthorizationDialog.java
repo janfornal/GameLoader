56733529c75fdbf53698051e170341b271774df9
janfornal <jan.fornal@student.uj.edu.pl>
2022-04-25 22:29:05 +0200

    Added authorization dialog class

---
+++ src/main/java/GameLoader/client/AuthorizationDialog.java
@@ -0,0 +1,24 @@
+package GameLoader.client;
+
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class AuthorizationDialog {
+    private AtomicReference<String> username = new AtomicReference<>("");
+    private TextInputDialog dialog;
+
+    AuthorizationDialog() {
+        dialog = new TextInputDialog();
+        dialog.setTitle("Authorization");
+        dialog.setHeaderText("Enter your name:");
+        dialog.setContentText("Name:");
+    }
+
+    String getUsername() {
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(username::set);
+        return username.get();
+    }
+}
