2f2da81976495b7d76923771b36508270a0fdd64
janfornal <jan.fornal@student.uj.edu.pl>
2022-04-29 15:11:13 +0200

    Few fields changed, processMessage expanded

--- src/main/java/GameLoader/client/Client.java
+++ src/main/java/GameLoader/client/Client.java
@@ -1,26 +1,51 @@
 package GameLoader.client;
 
-import GameLoader.common.AbstractService;
-import GameLoader.common.Connection;
-import GameLoader.common.Game;
-import GameLoader.common.Message;
+import GameLoader.common.*;
+import javafx.collections.FXCollections;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.Socket;
 
 public class Client implements AbstractService {
-    ViewModel currentGame;
+    private ViewModel currentModel;
+    private final Stage currentStage;
+    private final Connection activeConnection;
+    private Game.GameInfo chosenGame;
+    public PlayerInfo username;
 
-    Client() {
+    Client(Stage stage) throws IOException {
+        currentStage = stage;
+        activeConnection = new Connection(Client.this);
+    }
+
+    public void setCurrentModel(ViewModel Model) {
+        currentModel = Model;
+    }
+
+    public void setChosenGame(Game.GameInfo game) {
+        chosenGame = game;
     }
 
     void gameEnded() {
-
     }
 
     @Override
-    public void processMessage(Message.Any m, Connection c) {
-
+    public void processMessage(Message.Any message, Connection c) {
+        if(message instanceof Message.Authorization messageCast)
+            return;
+        if(message instanceof Message.RoomList messageCast && currentModel instanceof MenuViewModel currentGameCast)
+            currentGameCast.getElements().roomTableView().setItems(FXCollections.observableArrayList(messageCast.rooms()));
+        if(message instanceof Message.StartGame && currentModel instanceof MenuViewModel currentGameCast) {
+            if(chosenGame.equals("Dots and Boxes")) {
+                DotsAndBoxes starterInstance = new DotsAndBoxes().createNewGame();
+                currentModel = new DotsAndBoxesViewModel(this, starterInstance);
+            }
+            if(chosenGame.equals("Tic Tac Toe")) {
+                // repeat above;
+            }
+            ClientGUI.switchStage(currentStage, currentModel);
+        }
     }
 
     @Override
@@ -31,5 +56,12 @@ public class Client implements AbstractService {
     @Override
     public void reportConnectionClosed(Connection connection) {
 
+    }
+
+    public void sendMessage(Message.Any message) {
+        activeConnection.sendMessage(message);
+        if(message instanceof Message.Authorization authMessage) {
+            username = new PlayerInfo(authMessage.name());
+        }
     }
 }
