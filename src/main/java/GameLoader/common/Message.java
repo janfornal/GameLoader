ff6b2e6c6b9ccbf187fe3174af6ddf29104d4ebe
janfornal <jan.fornal@student.uj.edu.pl>
2022-04-29 15:08:37 +0200

    CreateRoom fields changed

--- src/main/java/GameLoader/common/Message.java
+++ src/main/java/GameLoader/common/Message.java
@@ -7,7 +7,7 @@ public interface Message extends Serializable {
     interface Any extends Serializable {}
     record Error(String cause) implements Any {}
     record Authorization(String name) implements Any {}
-    record CreateRoom(Class<? extends Game> game) implements Any {} // przesyłamy klasę gry którą chcemy stworzyć
+    record CreateRoom(Game.GameInfo game) implements Any {} // przesyłamy klasę gry którą chcemy stworzyć
     record GameList() implements Any {} // to jest na razie nieistotne
     record GetGamesList(String player) implements Any {} // to jest na razie nieistotne
     record RoomList(ArrayList<? extends Game.GameInfo> rooms) implements Any {} // przesyłamy listę obiektów typu GameInfo które mówią o nierozpoczętych jeszcze grach
