2414ff094a6d88f56feeddeed5fd59539a287c3b
janfornal <jan.fornal@student.uj.edu.pl>
2022-04-29 15:09:06 +0200

    Interface methods added

--- src/main/java/GameLoader/common/Game.java
+++ src/main/java/GameLoader/common/Game.java
@@ -5,7 +5,14 @@ import java.io.Serializable;
 public interface Game {
     interface Command extends Serializable {
 
+
     }
+
+    public Game createNewGame();
+    void makeMove(Command cmd);
+    boolean isLegal(Command cmd);
+    PlayerInfo[] players();
+    GameInfo getGameInfo();
 
     interface GameInfo extends Serializable {
         String getInfo();
