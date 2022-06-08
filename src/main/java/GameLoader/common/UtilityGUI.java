e1eb51ab9cd92eca23cba07539d9d26a09a2d4dc
Kacper Topolski <kacpertopolski@op.pl>
2022-06-08 19:04:19 +0200

    Font experiment

--- src/main/java/GameLoader/common/UtilityGUI.java
+++ src/main/java/GameLoader/common/UtilityGUI.java
@@ -1,11 +1,14 @@
 package GameLoader.common;
 
 import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.Property;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Pos;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Priority;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+
+import java.util.*;
 
 public interface UtilityGUI {
     class HBoxFixedRatio extends HBox {
@@ -22,4 +25,19 @@ public interface UtilityGUI {
             setHgrow(pain, Priority.ALWAYS);
         }
     }
+
+    Map<Double, Font> steelCityComicCache = new HashMap<>();
+    String steelCityComicURL = UtilityGUI.class.getResource("/steelCityComic.ttf").toExternalForm();
+
+    static Font getSteelCityComic(double sz) {
+        if (!steelCityComicCache.containsKey(sz))
+            steelCityComicCache.put(sz, Font.loadFont(steelCityComicURL, sz));
+        return steelCityComicCache.get(sz);
+    }
+
+    static void toSteelCityComic(List<Property<Font>> properties) {
+        for (Property<Font> p : properties)
+            p.setValue(getSteelCityComic(p.getValue().getSize()));
+    }
+
 }
