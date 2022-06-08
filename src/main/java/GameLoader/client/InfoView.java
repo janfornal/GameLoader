076468e44509842cdc0c5f7b0a322886765f9594
Kacper Topolski <kacpertopolski@op.pl>
2022-06-08 22:01:21 +0200

    Added info whose move is next

--- src/main/java/GameLoader/client/InfoView.java
+++ src/main/java/GameLoader/client/InfoView.java
@@ -3,10 +3,14 @@ package GameLoader.client;
 import static GameLoader.common.Serializables.*;
 import static GameLoader.common.Messages.*;
 
+import GameLoader.common.Game;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.text.Text;
+
+import java.util.List;
 
 public class InfoView {
     private final InfoViewModel ivm;
@@ -32,27 +36,39 @@ public class InfoView {
         splitPane.getItems().set(0, ivm.createGameView());
         splitPane.setDividerPosition(0, divide);
 
-        PlayerInfo userInfo = ivm.getPlayers().get(ivm.playingAs());
-        user.setText(userInfo.name());
-        userElo.setText("Elo: " + userInfo.elo());
+        int playingAs = ivm.playingAs();
 
-        PlayerInfo opponentInfo = ivm.getPlayers().get(1 - ivm.playingAs());
-        opponent.setText(opponentInfo.name());
-        opponentElo.setText("Elo: " + opponentInfo.elo());
+        List<PlayerInfo> info = ivm.getPlayers();
+        List<Text> names = playingAs == 0 ? List.of(user, opponent) : List.of(opponent, user);
+        List<Text> elos = playingAs == 0 ? List.of(userElo, opponentElo) : List.of(opponentElo, userElo);
 
+        for (int i = 0; i < 2; ++i) {
+            elos.get(i).setText("Elo: " + info.get(i).elo());
+            int finalI = i;
+            names.get(i).textProperty().bind(Bindings.createObjectBinding(
+                    () -> {
+                        if (ivm.getGame().getState() == Game.state.UNFINISHED && ivm.getGame().getTurn() == finalI)
+                            return "> " + info.get(finalI).name() + " <";
+                        else
+                            return info.get(finalI).name();
+                    }, ivm.getObservable()
+            ));
+        }
+
+        String opponentName = info.get(1 - playingAs).name();
         ChatManager ch = ivm.getModelUser().chatManager;
 
         textArea.textProperty().bind(
-                ch.get(opponentInfo.name())
+                ch.get(opponentName)
         );
 
         textField.setOnAction(event -> {
             String txt = textField.getText();
             if (txt.length() == 0)
                 return;
-            ch.update(opponentInfo.name(), ivm.getModelUser().username, txt);
+            ch.update(opponentName, ivm.getModelUser().username, txt);
             ivm.getModelUser().sendMessage(
-                    new ChatMessageToServer(txt, opponentInfo.name())
+                    new ChatMessageToServer(txt, opponentName)
             );
             textField.clear();
         });
